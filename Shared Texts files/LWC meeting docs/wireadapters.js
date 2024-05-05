f12edf75bc7d9ba9160096b79aa732476b864179
NavanitKamble <135243397+NavanitKamble@users.noreply.github.com>
2024-05-05 19:57:21 +0200

    dawdawdaw

--- Shared Texts files/LWC meeting docs/wireadapters.js
+++ Shared Texts files/LWC meeting docs/wireadapters.js
@@ -15,7 +15,7 @@ import NAME_FIELD from "@salesforce/schema/Contact.Name";
 func(data,error){
     //do stuff with data 
     //set the class properties in this function.
-    
+
 }
 
 
@@ -29,3 +29,78 @@ func(data,error){
       this.record = undefined;
     }
   }
+
+  --------------------calling apex ------------------------------------
+
+  import apexMethodName from "@salesforce/apex/namespace.classname.apexMethodReference";
+
+
+  public with sharing class ContactController {
+    @AuraEnabled(cacheable=true)
+    
+    public static List<Contact> getContactList() {
+        return [
+            SELECT Id, Name, Title, Phone, Email, Picture__c
+            FROM Contact
+            WHERE Picture__c != null
+            WITH SECURITY_ENFORCED
+            LIMIT 10
+        ];
+    }
+}
+
+import apexMethodName from "@salesforce/apex/namespace.ContactController.getContactList";
+
+/// waht is custom apex data type 
+
+ContactController{
+
+    wrapper {
+        field 1
+        field 2 
+    }
+}
+//can also be written as 
+Wrapper {
+    field 1
+    field 2
+}
+
+ContactController{
+
+    Wrapper obj = new Wrapper();
+}
+------------------contuning -----------------------------------
+
+import apexMethodName from '@salesforce/apex/namespace.classname.apexMethodReference';
+@wire(apexMethodName, { apexMethodParams })
+propertyOrFunction;
+
+
+
+import { LightningElement, wire } from 'lwc';
+import getContactList from '@salesforce/apex/ContactController.getContactList';
+
+export default class ApexWireMethodToProperty extends LightningElement {
+    @wire(getContactList) 
+    contacts;
+}
+
+
+
+---imperative call-------------------------------- 
+async handleApexMethod() {
+    try {
+      this.records = await getApexMethod();
+      this.error = undefined;
+    } catch (error) {
+      this.records = undefined;
+      this.error = error;
+    }
+}
+
+-------------------------referesh cache 
+refreshApex() from @salesforce/apex. used on both apex @wire and imperative 
+ // Notify LDS that you've changed the record outside its mechanisms
+      // Await the Promise object returned by notifyRecordUpdateAvailable()
+notifyRecordUpdateAvailable 
