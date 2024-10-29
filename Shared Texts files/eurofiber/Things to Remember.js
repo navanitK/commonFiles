81232d31c9f7b33a58ea6a2103b8305033e4f46e
NAVANIT KAMBLE <navanit.kamble@expereo.com>
2024-10-29 18:34:40 +0100

    hidden stuff

--- Shared Texts files/eurofiber/Things to Remember.js
+++ Shared Texts files/eurofiber/Things to Remember.js
@@ -1,3 +1,5 @@
 {
-	"Office Git hub": "g3cexsn5mw4b3ucv2fxmxqfufnpickpp2frwlz52oamvp7c5kqca"
+	"Office Git hub": "g3cexsn5mw4b3ucv2fxmxqfufnpickpp2frwlz52oamvp7c5kqca",
+	"home pc vs code": "kxj7h6ea6ypphbxvedn4yyc5rqewmizpavric43zwtr7jzsut7na",
+	"home pc git": "m54xhacpf2bde2i4aikswfzsoz4deryaf74vvnixp3ybjye6le4a"
 }
